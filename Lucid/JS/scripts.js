d9c3a7144f0b28b187fec6b75f194d17e1848bb2
JaneGM <janegm61@gmail.com>
2020-05-11 19:36:47 +0300

    меню-бургер

--- Lucid/JS/scripts.js
+++ Lucid/JS/scripts.js
@@ -1,6 +1,26 @@
+//Menu
 $( function() {
     $( ".header_burger" ).click("click", function() {
         $( ".header_burger,.header_menu" ).toggleClass( "active");
         $('.body').toggleClass('lock');
     });
 } );
+// Carousel
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+            800:{
+                items:2
+            },
+        }
+    });
+});
